8d4fceeae03aba5af42800d2a2dc0bfb38924fcd
dylanmcca <dylanmcca@gmail.com>
2026-02-27 21:10:07 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SrOzuKuKk9NbcOg9QhVFh502tGF6uac50QrDk8wFSVB2jDw4aW8LVLHUMZGoXO5EV2jpZxGELNVjlhLAuaVcSL7004dGhzXEt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
