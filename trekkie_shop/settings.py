80082ca40c6835b8efd2162d7b1926de2aca39d4
dylanmcca <dylanmcca@gmail.com>
2026-03-04 22:24:19 +0000

    Added checkout form data caching in payment intent

--- trekkie_shop/settings.py
+++ trekkie_shop/settings.py
@@ -26,9 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&t*7tl0(5)lk7*nk-duwkcbo8t_4*!787349qgw=li!heu@l@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost', # vs code preview
+    '127.0.0.1', # listen for stripe webhooks
+]
 
 
 # Application definition
@@ -176,6 +179,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
