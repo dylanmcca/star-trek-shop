a9df91b3b23028705337ec20dbf2937ee05ff403
dylanmcca <dylanmcca@gmail.com>
2026-03-11 20:40:54 +0000

    Added profile form, views and updated template

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#aab4ff');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#aab4ff');
+    } else {
+        $(this).css('color', '#f5c542');
+    }
+});
