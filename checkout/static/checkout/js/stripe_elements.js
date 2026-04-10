8a26f76ea6724ec3b3f0802cdbe9d9b1cc9f68b5
dylanmcca <dylanmcca@gmail.com>
2026-04-10 18:50:00 +0100

    did css validations & added pics of same for readme

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,6 +6,8 @@
     https://stripe.com/docs/stripe-js
 */
 
+/* global $, Stripe */
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -116,5 +118,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
